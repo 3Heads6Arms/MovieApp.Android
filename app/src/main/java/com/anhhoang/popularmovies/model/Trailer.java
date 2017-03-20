f56ef1cee2c1bc4eda6d281f78adef18b8dfd18c
Anh Hoang <h.anh.viet91@gmail.com>
2017-03-20 21:55:52 +0200

    feat: updated Trailer & TrailerResponse models

--- app/src/main/java/com/anhhoang/popularmovies/model/Trailer.java
+++ app/src/main/java/com/anhhoang/popularmovies/model/Trailer.java
@@ -8,4 +8,44 @@ package com.anhhoang.popularmovies.model;
  */
 
 public class Trailer {
+    private String id;
+    private String iso6391;
+    private String iso31161;
+    private String key;
+    private String name;
+    private String site;
+    private int size;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getIso6391() {
+        return iso6391;
+    }
+
+    public String getIso31161() {
+        return iso31161;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
