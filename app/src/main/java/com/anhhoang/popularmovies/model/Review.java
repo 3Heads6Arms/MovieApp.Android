d78d95c4f5f19362e21b88dc427473c74686bad4
Anh Hoang <h.anh.viet91@gmail.com>
2017-03-20 21:48:49 +0200

    feat: updated Review & Review Response models

--- app/src/main/java/com/anhhoang/popularmovies/model/Review.java
+++ app/src/main/java/com/anhhoang/popularmovies/model/Review.java
@@ -18,4 +18,20 @@ public class Review {
     private String content;
     @SerializedName("url")
     private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
